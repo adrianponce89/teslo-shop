03bd135176e5240e560d48e9f315049743105b70
Adrian Ponce <adrian.ponce.89@gmail.com>
2023-12-15 18:01:43 -0300

    Insert mock data by seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
   constructor(private readonly productsService: ProductsService) {}
   async executeSeed() {
     await this.productsService.deleteAllProducts();
-    return `This action execute seed`;
+    const products = initialData.products;
+    const insertPromises = products.map((product) =>
+      this.productsService.create(product),
+    );
+
+    await Promise.all(insertPromises);
+
+    return `Seeded ${products.length} products`;
   }
 }
