b70068afd09eee55ae35812eec1caa7ec7c6dc84
Adrian Ponce <adrian.ponce.89@gmail.com>
2023-12-15 20:37:04 -0300

    Save file to file system

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -7,20 +7,30 @@ import {
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from './helpers/fileFilter';
+import { fileFilter, fileNamer } from './helpers';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: { fileSize: 1024 * 1024 * 5 },
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('Make sure to send a image file');
     }
     return {
-      fileName: file?.originalname,
+      fileName: file.filename,
     };
   }
 }
