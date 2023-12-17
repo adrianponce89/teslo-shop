8d7625d6632521bee4892c680ee4132fbd0143a2
Adrian Ponce <adrian.ponce.89@gmail.com>
2023-12-17 15:10:18 -0300

    Use Auth decorator in other modules

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('seed')
+@Auth(ValidRoles.ADMIN) // Here we are using the Auth decorator to protect all routes in controller
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
